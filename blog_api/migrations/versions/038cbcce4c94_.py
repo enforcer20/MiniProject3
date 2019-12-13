8ea0bc3beb8bc74f012b77028a62cce37866e93d
Karan <kwalanj@gmail.com>
2019-12-13 03:15:25 -0500

    Fix: formatting change

--- blog_api/migrations/versions/038cbcce4c94_.py
+++ blog_api/migrations/versions/038cbcce4c94_.py
@@ -8,7 +8,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '037cbcce4c67'
+revision = '038cbcce4c94'
 down_revision = None
 branch_labels = None
 depends_on = None
