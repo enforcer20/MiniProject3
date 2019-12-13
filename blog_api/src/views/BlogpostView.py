284cf708e3ba1b4a5ccef0abbe828934e20874b8
Karan <kwalanj@gmail.com>
2019-12-13 02:51:17 -0500

    Feature: Added  put and delete method to blogpostview

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -35,6 +35,18 @@ def get_all():
     return custom_response(data, 200)
 
 
+@blogpost_api.route('/<int:blogpost_id>', methods=['GET'])
+def get_one(blogpost_id):
+  """
+  Get A Blogpost
+  """
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  return custom_response(data, 200)
+
+
 def custom_response(res, status_code):
     """
   Custom Response Function
@@ -44,3 +56,43 @@ def custom_response(res, status_code):
         response=json.dumps(res),
         status=status_code
     )
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@Auth.auth_required
+def update(blogpost_id):
+    """
+    Update A Blogpost
+    """
+    req_data = request.get_json()
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
+
+    data, error = blogpost_schema.load(req_data, partial=True)
+    if error:
+        return custom_response(error, 400)
+    post.update(data)
+
+    data = blogpost_schema.dump(post).data
+    return custom_response(data, 200)
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['DELETE'])
+@Auth.auth_required
+def delete(blogpost_id):
+    """
+    Delete A Blogpost
+    """
+    post = BlogpostModel.get_one_blogpost(blogpost_id)
+    if not post:
+        return custom_response({'error': 'post not found'}, 404)
+    data = blogpost_schema.dump(post).data
+    if data.get('owner_id') != g.user.get('id'):
+        return custom_response({'error': 'permission denied'}, 400)
+
+    post.delete()
+    return custom_response({'message': 'deleted'}, 204)
